55386dbcd0ce697e6833b5ab633a8c5b105b89f7
Sahan <sahan1989@gmail.com>
2016-04-15 15:00:58 +0530

    Page Redirecting and View

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ Create Sample Shopping Cart Application for home work assignment in the Zaizi As
 * MySQL
 * Spring
 * Hibernate
-* HTML , CSS & Javascript(AngularJS)
+* HTML , CSS & Javascript (AngularJS/Bootstrap)
 
 ## *Build Project*
 1. Create Database ```CREATE DATABASE zaizidb;```
-2. Clone the project
-3. Navigate to root path (ShoppingCart)
-4. Invoke ```bower install```
-5. Invoke ```mvn clean install -DskipTests```
-6. Navigate to target folder
+2. Set Username and Password in the ```application.properties``` file
+3. Clone the project
+4. Navigate to root path (ShoppingCart)
+5. Invoke ```bower install```
+6. Invoke ```mvn clean install -DskipTests```
+7. Navigate to target folder
 
 ## *Run Project*
 * Invoke following command
 ```java -jar ShoppingCart-0.0.1-SNAPSHOT.jar```
-navigate to http://localhost:8080/shoppingcart/
+navigate to ```http://localhost:8080/shoppingcart/```
--- src/main/java/com/sahan/zaizi/service/ProductService.java
+++ src/main/java/com/sahan/zaizi/service/ProductService.java
@@ -23,10 +23,15 @@ public class ProductService{
 
     static {
         products.add(new Product(35.75d,1000,"Pears baby soap for Kids","Soap"));
-        products.add(new Product(45.50d,500,"Signal Tooth Brushes Size in (L, M, S)","Tooth Brushes"));
-        products.add(new Product(1500.0d,100,"Casual Shirt imported from France","Cloths"));
-        products.add(new Product(1000.0d,400,"Leather bag imported from USA ","Bags"));
-        products.add(new Product(450.0d,800,"Hot Water Bottles","Bottles"));
+        products.add(new Product(45.50d,500,"Signal Tooth Brushes Size in (L, M, S)","Tooth Brushe"));
+        products.add(new Product(1500.0d,100,"Casual Shirt imported from France","Shirt"));
+        products.add(new Product(1000.0d,400,"Leather bag imported from USA ","Office Bag"));
+        products.add(new Product(450.0d,800,"Hot Water Bottles","Bottle"));
+        products.add(new Product(2500.0d,800,"Imported wrist watches from swiss","Wrist Watch"));
+        products.add(new Product(45000.0d,800,"3G/4G capability","Mobile Phone"));
+        products.add(new Product(300.0d,800,"Head and Shoulders Shampoo","Shampoo"));
+        products.add(new Product(550.0d,800,"Imported Leather Wallets from AUS","Leather Wallets"));
+        products.add(new Product(85000.0d,800,"Imported Canon camera from USA","Camera"));
     }
 
     public void saveInitialBatch(){
